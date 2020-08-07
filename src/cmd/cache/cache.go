ede5d04a3effe8bc48f0268ff55ead543e637c22
Trọng Đức <v.ducht1@vinid.net>
2020-08-07 17:34:19 +0700

    Add tracing in multi cache

--- src/cmd/cache/cache.go
+++ src/cmd/cache/cache.go
@@ -1,8 +1,10 @@
 package cache
 
 import (
+	"context"
 	"github.com/1infras/go-kit/src/cmd/cache/lru"
 	"github.com/go-redis/redis"
+	"go.elastic.co/apm"
 	"reflect"
 	"sync"
 	"time"
@@ -39,22 +41,46 @@ func NewMultiCache(size int, expiration time.Duration, redisCache redis.Universa
 	}, nil
 }
 
-//Set
-//Set cache with value and expiration
-func (c *MultiCache) Set(key string, values []byte, expiration time.Duration) (bool, error) {
-	//Set to redis first
-	err := c.Redis.Set(key, values, expiration).Err()
-	if err != nil {
-		return false, err
-	}
-
-	//Set to LRU
-	c.LRU.Add(key, values, expiration)
-
-	return true, nil
+//Set cache to redis
+func (c *MultiCache) redisSet(ctx context.Context, key string, values []byte, expiration time.Duration) error {
+	span, _ := apm.StartSpan(ctx, "redis.set", "cache.multi_cache")
+	defer span.End()
+	return c.Redis.Set(key, values, expiration).Err()
 }
 
-//Get
+//Set cache to LRU
+func (c *MultiCache) lruSet(ctx context.Context, key string, values []byte, expiration time.Duration) bool {
+	span, _ := apm.StartSpan(ctx, "lru.set", "cache.multi_cache")
+	defer span.End()
+	return c.LRU.Add(key, values, expiration)
+}
+
+//Get cache from redis
+func (c *MultiCache) redisGet(ctx context.Context, key string) ([]byte, error) {
+	span, _ := apm.StartSpan(ctx, "redis.get", "cache.multi_cache")
+	defer span.End()
+	return c.Redis.Get(key).Bytes()
+}
+
+//Get ttl cache from redis
+func (c *MultiCache) redisGetTTL(ctx context.Context, key string) (time.Duration, error) {
+	span, _ := apm.StartSpan(ctx, "redis.get_ttl", "cache.multi_cache")
+	defer span.End()
+	return c.Redis.TTL(key).Result()
+}
+
+//Get cache from lru
+func (c *MultiCache) lruGet(ctx context.Context, key string) (interface{}, bool) {
+	span, _ := apm.StartSpan(ctx, "lru.get", "cache.multi_cache")
+	defer span.End()
+	return c.LRU.Get(key)
+}
+
+//Set cache with value and expiration
+func (c *MultiCache) Set(key string, values []byte, expiration time.Duration) (bool, error) {
+	return c.SetCtx(nil, key, values, expiration)
+}
+
 //Get cache with multiple layers (LRU first, backed by Redis)
 func (c *MultiCache) Get(key string) ([]byte, error) {
 	//Get from LRU
@@ -64,17 +90,61 @@ func (c *MultiCache) Get(key string) ([]byte, error) {
 		return v.Bytes(), nil
 	}
 
+	//Get from Redis
 	v, err := c.Redis.Get(key).Bytes()
 	if err != nil {
 		return nil, err
 	}
 
+	//Get TTL from Redis
 	ttl, err := c.Redis.TTL(key).Result()
 	if err != nil {
 		return nil, err
 	}
 
+	//Set back to LRU with TTL
 	c.LRU.Add(key, v, ttl)
+
+	return v, nil
+}
+
+//SetCtx cache with value and expiration, support transaction to tracing
+func (c *MultiCache) SetCtx(ctx context.Context, key string, values []byte, expiration time.Duration) (bool, error) {
+	//Set to redis first
+	err := c.redisSet(ctx, key, values, expiration)
+	if err != nil {
+		return false, err
+	}
+
+	//Set to LRU
+	c.lruSet(ctx, key, values, expiration)
+
+	return true, nil
+}
+
+//Get cache with multiple layers (LRU first, backed by Redis), support transaction to tracing
+func (c *MultiCache) GetCtx(ctx context.Context, key string) ([]byte, error) {
+	//Get from LRU
+	values, ok := c.lruGet(ctx, key)
+	if ok {
+		v := reflect.ValueOf(values)
+		return v.Bytes(), nil
+	}
+
+	//Get from Redis
+	v, err := c.redisGet(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	//Get TTL from Redis
+	ttl, err := c.redisGetTTL(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	//Set back to LRU with TTL
+	c.lruSet(ctx, key, v, ttl)
 
 	return v, nil
 }
--- src/cmd/database/elasticsearch/elastic_apm.go
+++ src/cmd/database/elasticsearch/elastic_apm.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+const (
+	//DefaultAPMServer
+	DefaultAPMServer = "http://localhost:8200"
+)
+
 //APMConnection - Connection to APM Server
 type APMConnection struct {
 	URL         string `json:"url"`
@@ -25,10 +30,28 @@ func APMConnectionWithViper() *APMConnection {
 	}
 }
 
-func APMEnvironment(c *APMConnection) {
-	os.Setenv("ELASTIC_APM_SERVER_URL", c.URL)
-	os.Setenv("ELASTIC_APM_SECRET_TOKEN", c.SecretToken)
-	os.Setenv("ELASTIC_APM_SERVICE_NAME", c.ServiceName)
-	os.Setenv("ELASTIC_APM_ENVIRONMENT", c.Environment)
+func DefaultAPMConnection() *APMConnection {
+	return &APMConnection{
+		URL: DefaultAPMServer,
+	}
+}
+
+func (c *APMConnection) AutoBindEnvironment() {
+	if c.URL != "" {
+		os.Setenv("ELASTIC_APM_SERVER_URL", c.URL)
+	}
+
+	if c.SecretToken != "" {
+		os.Setenv("ELASTIC_APM_SECRET_TOKEN", c.SecretToken)
+	}
+
+	if c.ServiceName != "" {
+		os.Setenv("ELASTIC_APM_SERVICE_NAME", c.ServiceName)
+	}
+
+	if c.Environment != "" {
+		os.Setenv("ELASTIC_APM_ENVIRONMENT", c.Environment)
+	}
+
 	os.Setenv("ELASTIC_APM_ACTIVE", cast.ToString(c.Active))
 }
